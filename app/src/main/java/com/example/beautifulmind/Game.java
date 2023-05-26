3a0a6adc0bc7620cd7ea4629e19c5727f917b2dc
ronasdev <ronasdev@gmail.com>
2023-05-26 09:27:38 +0200

    first version finished of the app

--- app/src/main/java/com/example/beautifulmind/Game.java
+++ app/src/main/java/com/example/beautifulmind/Game.java
@@ -2,8 +2,12 @@ package com.example.beautifulmind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,10 +18,32 @@ public class Game extends AppCompatActivity {
     private String opponentType = "";
     private final static int MIN_MATRIX = -5;
     private final static int MAX_MATRIX = 5;
-    private TextView utility11 = null;
-    private TextView utility12 = null;
-    private TextView utility21 = null;
-    private TextView utility22 = null;
+    private TextView utilityAA = null;
+    private TextView utilityAB = null;
+    private TextView utilityBA = null;
+    private TextView utilityBB = null;
+
+    //buttons of different actions
+    private Button aActionBtn = null;
+    private Button bActionBtn = null;
+
+    //Know if all buttons of action is clicking
+    private boolean opponentActionIsChoosed = false;
+    private int numberOfActionsButtonClicked = 0;
+
+    private String myAction = "_";
+    private String opponentAction = "_";
+
+    private TextView myChoosedAction =null;
+    private TextView opponentChoosedAction = null;
+
+    private String myObtain = "";
+    private String opponentObtain = "";
+
+    private TextView myPoint = null;
+    private TextView opponentPoint = null;
+
+    private Button dismissButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +52,145 @@ public class Game extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         opponentType = extras.getString("opponentType","Random");
+        TextView strategyName = (TextView) findViewById(R.id.strategy_name);
+        strategyName.setText(opponentType);
+
 
         initWidgets();
 
         //Toast.makeText(getApplicationContext(),generateRandomUtilityMatrix(),Toast.LENGTH_SHORT).show();
         fillGameTable();
+
+        aActionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberOfActionsButtonClicked++;
+                if(!opponentActionIsChoosed){
+                    myAction = "A";
+                    if(opponentType.equals("Nash")){
+                        String[] utilities = null;
+                        utilities[0] =  utilityAA.getText().toString();
+                        utilities[1] =  utilityAB.getText().toString();
+                        utilities[2] =  utilityBA.getText().toString();
+                        utilities[3] =  utilityBB.getText().toString();
+
+                        for (String utility : utilities) {
+                            String[] parts = utility.split("/");
+                            System.out.println(parts);
+                            /*if (parts[0].equals(parts[1])){
+                                Log.d("nash", "Equlibre de nash");
+                            }*/
+                        }
+
+                    }
+                    opponentRandomAction();
+                }
+                if (opponentActionIsChoosed && numberOfActionsButtonClicked == 2){
+                    aActionBtn.setEnabled(false);
+                    bActionBtn.setEnabled(false);
+
+                    myChoosedAction.setText("Action "+myAction);
+                    opponentChoosedAction.setText("Action "+opponentAction);
+
+                //    if (opponentType.equals("Random")){
+                        String[] utility = null;
+                        if (myAction.equals("A") ){
+                            if (opponentAction.equals("A")){
+                                utilityAA.setBackgroundColor(getResources().getColor(R.color.OBTAIN_CELL_BG));
+                                utility = utilityAA.getText().toString().split("/");
+                                myObtain = utility[0].trim();
+                                opponentObtain = utility[1].trim();
+
+                                if (opponentType.equals("Nash")){
+                                    String equilibriumNash = "There is a unique Nash equilibrium in this game: \n" +
+                                            "My action: "+myAction +
+                                            "\nOpponent action: "+opponentAction;
+                                    Toast.makeText(getApplicationContext(),equilibriumNash,Toast.LENGTH_LONG).show();
+                                }
+
+                            } else if(opponentAction.equals("B")){
+                                utilityAB.setBackgroundColor (getResources().getColor(R.color.OBTAIN_CELL_BG));
+                                utility = utilityAB.getText().toString().split("/");
+                                myObtain = utility[0].trim();
+                                opponentObtain = utility[1].trim();
+                                }
+                            }
+                        }
+
+                    if (!myObtain.isEmpty() && !opponentObtain.isEmpty()){
+                        myPoint.setText(myObtain+" points");
+                        opponentPoint.setText(opponentObtain+" points");
+                    }
+
+            //    }
+            }
+        });
+
+        bActionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberOfActionsButtonClicked++;
+                if(!opponentActionIsChoosed){
+                    myAction = "B";
+                    opponentRandomAction();
+                }
+                if (opponentActionIsChoosed && numberOfActionsButtonClicked == 2){
+                    aActionBtn.setEnabled(false);
+                    bActionBtn.setEnabled(false);
+
+                    myChoosedAction.setText("Action "+myAction);
+                    opponentChoosedAction.setText("Action "+opponentAction);
+
+                   // if (opponentType.equals("Random")){
+                        String[] utility = null;
+                        if (myAction.equals("B") ){
+                            if (opponentAction.equals("A")){
+                                utilityBA.setBackgroundColor(getResources().getColor(R.color.OBTAIN_CELL_BG));
+                                utility = utilityBA.getText().toString().split("/");
+                                myObtain = utility[0].trim();
+                                opponentObtain = utility[1].trim();
+
+                                if (opponentType.equals("Nash")){
+                                    String equilibriumNash = "There is a unique Nash equilibrium in this game: \n" +
+                                            "My action: "+myAction +
+                                            "\nOpponent action: "+opponentAction;
+                                    Toast.makeText(getApplicationContext(),equilibriumNash,Toast.LENGTH_LONG).show();
+                                }
+
+                            } else if(opponentAction.equals("B")){
+                                utilityBB.setBackgroundColor(getResources().getColor(R.color.OBTAIN_CELL_BG));
+                                utility = utilityBB.getText().toString().split("/");
+                                myObtain = utility[0].trim();
+                                opponentObtain = utility[1].trim();
+                            }
+                        }
+                //    }
+
+                    if (!myObtain.isEmpty() && !opponentObtain.isEmpty()){
+                        myPoint.setText(myObtain+" points");
+                        opponentPoint.setText(opponentObtain+" points");
+                    }
+                }
+            }
+        });
+
+        dismissButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent resultForMainIntent = new Intent(getApplicationContext(),MainActivity.class);
+
+                if(myObtain.isEmpty() && opponentObtain.isEmpty()){
+                    resultForMainIntent.putExtra("game_played",0);
+                    Toast.makeText(getApplicationContext(),"The game could not be finished", Toast.LENGTH_LONG).show();
+                }else{
+                    resultForMainIntent.putExtra("game_played",1);
+                    resultForMainIntent.putExtra("my_obtain",Integer.parseInt(myObtain));
+                    resultForMainIntent.putExtra("opponent_obtain",Integer.parseInt(opponentObtain));
+                }
+                setResult(RESULT_OK, resultForMainIntent);
+                finish();
+            }
+        });
 
 
     }
@@ -64,10 +224,10 @@ public class Game extends AppCompatActivity {
                  matrixTable[i] = matrix;
         }
 
-        utilityCell[0] = utility11;
-        utilityCell[1] = utility12;
-        utilityCell[2] = utility21;
-        utilityCell[3] = utility22;
+        utilityCell[0] = utilityAA;
+        utilityCell[1] = utilityAB;
+        utilityCell[2] = utilityBA;
+        utilityCell[3] = utilityBB;
 
         for(int i = 0; i < 4; i++){
             utilityCell[i].setText(matrixTable[i]);
@@ -75,11 +235,39 @@ public class Game extends AppCompatActivity {
 
     }
 
+    // opponent random action's choosing
+    private void opponentRandomAction(){
+        opponentActionIsChoosed = true;
+        int randomNumber = new Random().nextInt((2 - 1) + 1) +1;
+        if (randomNumber == 1){
+            aActionBtn.performClick();
+            opponentAction = "A";
+        }else if (randomNumber == 2){
+            bActionBtn.performClick();
+            opponentAction = "B";
+        }
+
+    }
+
     //Initialize all widget of activity
     private void initWidgets(){
-        utility11 = (TextView) findViewById(R.id.utility11);
-        utility12 = (TextView) findViewById(R.id.utility12);
-        utility21 = (TextView) findViewById(R.id.utility21);
-        utility22 = (TextView) findViewById(R.id.utility22);
+        // Initialisation of the cells of table game
+        utilityAA = findViewById(R.id.utilityAA);
+        utilityAB = findViewById(R.id.utilityAB);
+        utilityBA = findViewById(R.id.utilityBA);
+        utilityBB = findViewById(R.id.utilityBB);
+
+        //All action's Button initialisation
+        aActionBtn = findViewById(R.id.actionABtn);
+        bActionBtn = findViewById(R.id.actionBBtn);
+
+        myChoosedAction = findViewById(R.id.myAction);
+        opponentChoosedAction = findViewById(R.id.opponentAction);
+
+        myPoint = findViewById(R.id.myPoint);
+        opponentPoint = findViewById(R.id.opponentPoint);
+
+        dismissButton = findViewById(R.id.dismissBtn);
+
     }
 }
--- app/src/main/java/com/example/beautifulmind/MainActivity.java
+++ app/src/main/java/com/example/beautifulmind/MainActivity.java
@@ -1,22 +1,36 @@
 package com.example.beautifulmind;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnStartOver = null;
+    public final static int GO_GAME_REQUEST_CODE = 2;
+
     private RadioGroup opponentTypeBtnGroup = null;
     private Button btnGenerate = null;
+
+    private TextView numberGamePlayedText = null;
+    //private int numberGamePlayed = 0;
+    private TextView myTotalScoreCell = null;
+    private TextView opponentTotalScoreCell = null;
+    private Button btnStartOver = null;
+    private ImageView nashImage = null;
+
+    RadioButton opponentTypeRadioBtnSelected = null;
+
+    private int myTotalScore = 0;
+    private int opponentTotalScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,31 +40,84 @@ public class MainActivity extends AppCompatActivity {
         //initialization of different widgets
         initWidget();
 
-        // On clicking on generate button
+        opponentTypeBtnGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+
+                numberGamePlayedText.setText("0");
+                myTotalScoreCell.setText("0");
+                opponentTotalScoreCell.setText("0");
+
+                opponentTypeRadioBtnSelected = radioGroup.findViewById(checkedId);
+                if (radioGroup.indexOfChild(opponentTypeRadioBtnSelected) == 3){
+                    nashImage.setImageResource(R.drawable.john_forbes_nash);
+                }else {
+                    nashImage.setImageResource(0);
+                }
+            }
+        });
+
+        //clicking on generate button
         btnGenerate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(opponentTypeBtnGroup.getCheckedRadioButtonId() == -1) {
+                int checkedRadioBtnId = opponentTypeBtnGroup.getCheckedRadioButtonId();
+
+                if(checkedRadioBtnId == -1) {
                     Toast.makeText(MainActivity.this, "Select an opponent type first!",
                             Toast.LENGTH_LONG).show();
                 }else{
-                    RadioButton opponentTypeSelected = (RadioButton) findViewById(opponentTypeBtnGroup.getCheckedRadioButtonId());
-                    String opponentType = opponentTypeSelected.getText().toString();
+                    String opponentType = opponentTypeRadioBtnSelected.getText().toString();
 
-                    Intent intent = new Intent(getApplicationContext(),Game.class);
-                    intent.putExtra("opponentType",opponentType);
-                    startActivity(intent);
-                    finish();
+                    Intent startGameIntent = new Intent(getApplicationContext(),Game.class);
+                    startGameIntent.putExtra("opponentType",opponentType);
+                    startActivityForResult(startGameIntent, GO_GAME_REQUEST_CODE);
 
                 }
             }
         });
+
+        //Clicking start over button
+        btnStartOver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberGamePlayedText.setText("0");
+                myTotalScoreCell.setText("0");
+                opponentTotalScoreCell.setText("0");
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GO_GAME_REQUEST_CODE){
+            if (resultCode == RESULT_OK){
+                int numberGamePlayed =  Integer.parseInt((String) numberGamePlayedText.getText()) + data.getIntExtra("game_played",1);
+                numberGamePlayedText.setText(String.valueOf(numberGamePlayed));
+
+                int myObtain = Integer.parseInt((String) myTotalScoreCell.getText()) + data.getIntExtra("my_obtain",0);
+                int opponentObtain = Integer.parseInt((String) opponentTotalScoreCell.getText())+ data.getIntExtra("opponent_obtain",0);
+
+                myTotalScoreCell.setText(String.valueOf(myObtain));
+                opponentTotalScoreCell.setText(String.valueOf(opponentObtain));
+
+            }
+        }
+    }
+
 
     //Initialize all widget of activity
     private void  initWidget(){
-        btnGenerate = (Button) findViewById(R.id.btnGenerate);
-        btnStartOver = (Button) findViewById(R.id.btnStartOver);
-        opponentTypeBtnGroup = (RadioGroup) findViewById(R.id.opponentTypeBtnGroup);
+        btnGenerate = findViewById(R.id.btnGenerate);
+        opponentTypeBtnGroup = findViewById(R.id.opponentTypeBtnGroup);
+
+        numberGamePlayedText = findViewById(R.id.numberGamesPlayed);
+        myTotalScoreCell = findViewById(R.id.myTotalScoreCell);
+        opponentTotalScoreCell = findViewById(R.id.opponentTotalScoreCell);
+
+        nashImage = findViewById(R.id.nash_image);
+
+        btnStartOver = findViewById(R.id.btnStartOver);
     }
 }
